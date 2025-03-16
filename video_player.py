7eab963c38823fd722393da5effb8521647c6d4b
Stolan00 <sbelikove@gmail.com>
2025-03-15 19:13:08 -0700

    no longer crashes when closing mpv, restarting playback is possible

--- video_player.py
+++ video_player.py
@@ -1,17 +1,46 @@
 import mpv
+import threading
 
-# player = mpv.MPV(ytdl=True)
-# player.play('https://youtu.be/DOmdB7D-pUU')
-# player.wait_for_playback()
-
-class VideoPlayer():
+class VideoPlayer:
     def __init__(self):
         self.player = mpv.MPV(
             input_default_bindings=True,
             input_vo_keyboard=True,
             osc=True
-            )
-    
+        )
+        self.playback_thread = None
+        self._stop_event = threading.Event()
+
     def play_video(self, path: str):
-        self.player.play(path)
-        self.player.wait_for_playback()
+        # Stop any existing playback
+        self.stop()
+        
+        # Start new playback in a thread
+        self.playback_thread = threading.Thread(
+            target=self._play,
+            args=(path,),
+            daemon=True
+        )
+        self.playback_thread.start()
+
+    def _play(self, path: str):
+        try:
+            self.player.play(path)
+            while not self._stop_event.is_set() and self.player.core_idle:
+                self.player.wait_for_playback()
+        except mpv.ShutdownError as e:
+            print(f"MPV shutdown handled: {e}")
+        finally:
+            self._cleanup()
+
+    def stop(self):
+        if self.playback_thread and self.playback_thread.is_alive():
+            self._stop_event.set()
+            self.player.terminate()
+            self.playback_thread.join(timeout=1)
+            self._cleanup()
+
+    def _cleanup(self):
+        self._stop_event.clear()
+        if self.player:
+            self.player.terminate()
